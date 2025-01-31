6b6463e4b22ee15ed85595dc0a5e124ef63744e9
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-31 23:24:05 +0900

    FEAT: Rename datatype_val_nnn to out_val_nnn (+ add some comments)

--- include/arg/find.h
+++ include/arg/find.h
@@ -3,18 +3,43 @@
 
 #include "cond/val.h"
 
-typedef char* const     datatype_val_str;
-typedef char            datatype_val_char;
-typedef int             datatype_val_int;
-typedef char* const     datatype_val_filepath;
-typedef char* const     datatype_val_dirpath;
-typedef char* const     datatype_val_url;
+// ==== out types
+typedef char* const     out_val_str;
+typedef char            out_val_char;
+typedef int             out_val_int;
+typedef char* const     out_val_filepath;
+typedef char* const     out_val_dirpath;
+typedef char* const     out_val_url;
 
-void find_str_with_id(const unsigned long id, const datatype_val_str* const out);
-void find_char_with_id(const unsigned long id, const datatype_val_char* const out);
-void find_int_with_id(const unsigned long id, const datatype_val_int* const out);
-void find_filepath_with_id(const unsigned long id, const datatype_val_filepath* const out);
-void find_dirpath_with_id(const unsigned long id, const datatype_val_dirpath* const out);
-void find_url_with_id(const unsigned long id, const datatype_val_url* const out);
+// ==== API functions
+void find_str_with_id(
+    const unsigned long id,
+    const out_val_str* const out
+);
+
+void find_char_with_id(
+    const unsigned long id,
+    const out_val_char* const out
+);
+
+void find_int_with_id(
+    const unsigned long id,
+    const out_val_int* const out
+);
+
+void find_filepath_with_id(
+    const unsigned long id,
+    const out_val_filepath* const out
+);
+
+void find_dirpath_with_id(
+    const unsigned long id,
+    const out_val_dirpath* const out
+);
+
+void find_url_with_id(
+    const unsigned long id,
+    const out_val_url* const out
+);
 
 #endif // CCLI_ARG_FIND_H
