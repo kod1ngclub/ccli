23257b0184cf5e635c87d57668b212f2bf91fd2f
kod1ngclub <wiiwefwefbi@gmail.com>
2025-02-01 22:12:15 +0900

    BUILD: Write new version of make.py

--- make.py
+++ make.py
@@ -1,86 +1,225 @@
 import os;
 
-# clear CLI
-os.system('cls' if os.name == 'nt' else 'clear')
+# ==== config
+CONFIG_NAME     = "ccli"
+CONFIG_GUARD    = "CCLI_H"
+CONFIG_OUT_H    = "ccli.h"
+CONFIG_OUT_C    = "ccli.c"
+
+# ==== models
+class Lib:
+    def __init__(self, _head):
+        self.head = _head
+
+class Head:
+    def __init__(self, _file):
+        self.file = _file
+
+class Src:
+    def __init__(self, _file):
+        self.file = _file
+
+class Stage:
+    def __init__(self, _files):
+        self.files = _files
+        self.libs = []
+
+    def dep(self, _lib):
+        self.libs.append(Lib(_lib))
+        return self
 
 class Mod:
-    file        = ""
-    includable  = False
+    def __init__(self, _stages, _name):
+        self.stages = _stages
+        self.name = _name
 
-    def init(self, f):
-        self.file = f
-
-        print("Mod", f, "was added")
-        return self
-
-    def include(self, allowance):
-        self.includable = allowance
-
-        print("Mod", self.file, "set include-able")
-        return self
-
-print("==== Modules lv0")
-mod0 = [
-    Mod().init("include/arg/cond/flag.h"),
-    Mod().init("include/arg/cond/val.h"),
-    Mod().init("include/arg/cond/condset.h"),
-
-    Mod().init("include/arg/storage/datatype.h"),
-    Mod().init("include/arg/storage/flag.h"),
-    Mod().init("include/arg/storage/token.h"),
-    Mod().init("include/arg/storage/api.h")
-]
-
-print("==== Modules lv1")
-mod1 = [
-    Mod().init("include/arg/cond.h"),
-    Mod().init("include/arg/find.h"),
-    Mod().init("include/arg/init.h"),
-]
-
-print("==== Modules lv2")
-mod2 = [
-    Mod().init("include/input.h"),
-    Mod().init("include/password.h"),
-    Mod().init("include/select.h"),
-]
+# Lib       - external or standard libary
+# Head      - header
+# Src       - source
+# Stage     - collection of Head or Src having same dependency level
+# Mod       - module
 
 ENDIF = "#endif"
-def unguard(lines):
+def Unguard(lines):
     ifndefless = lines[2:]
     endifless = [line for line in ifndefless if not line.startswith(ENDIF)]
 
     return endifless
 
 INCLUDE = "#include"
-def uninclude(lines):
+def Uninclude(lines):
     return [line for line in lines if not line.startswith(INCLUDE)]
 
-mods    = mod0 + mod1 + mod2
+LEFTPAD_SIZE = 16
+def Leftpad(text):
+    return str(text).ljust(LEFTPAD_SIZE)
 
-EMPTY       = ""
-out         = ""
-for mod in mods:
-    with open(mod.file, "r") as file:
-        lines = file.readlines()
+def LogTable():
+    sec_modname     = Leftpad("module name")
+    sec_index       = Leftpad("stage level")
+    sec_type        = Leftpad("type")
+    sec_filename    = Leftpad("filepath")
+    
+    print(f"{sec_modname}{sec_index}{sec_type}{sec_filename}")
+    print("=" * LEFTPAD_SIZE * 4)
 
-        lines = unguard(lines)
-        if not mod.includable: lines = uninclude(lines)
+def LogFile(modname, index, filename):
+    sec_modname     = Leftpad(modname)
+    sec_index       = Leftpad(f"stage/{index}")
+    sec_type        = Leftpad("file")
+    sec_filename    = Leftpad(filename)
+    
+    print(f"{sec_modname}{sec_index}{sec_type}{sec_filename}")
 
-        out += EMPTY.join(lines)
+def LogLib(modname, index, libname):
+    sec_modname     = Leftpad(modname)
+    sec_index       = Leftpad(f"stage/{index}")
+    sec_type        = Leftpad("lib")
+    sec_libname     = Leftpad(libname)
+    
+    print(f"{sec_modname}{sec_index}{sec_type}{sec_libname}")
 
-NEWLINE             = "\n"
-FILEGUARD_IFNDEF    = "#ifndef CCLI" + NEWLINE
-FILEGUARD_DEFINE    = "#define CCLI" + NEWLINE
-FILEGUARD_ENDIF     = "#endif // CCLI" + NEWLINE
+def Newline():
+    print()
 
-OUT_FILENAME = "ccli.h"
-with open(OUT_FILENAME, "w") as file:
-    file.write(FILEGUARD_IFNDEF)
-    file.write(FILEGUARD_DEFINE)
-    file.write(out)
-    file.write(FILEGUARD_ENDIF)
+NEWLINE = "\n"
+EMPTY   = ""
+SPACE   = " "
+QUOTE   = "\""
 
-print()
+def BuildHead(mods: list[Mod]):
+    filepaths   = []
+    libnames    = []
 
-print("Write out to", OUT_FILENAME)
+    LogTable()
+    for mod in mods:
+        for index, stage in enumerate(mod.stages):
+            for head in stage.files:
+                filepaths.append(head.file)
+                LogFile(mod.name, index, head.file)
+
+            for lib in stage.libs:
+                libnames.append(lib.head)
+                LogLib(mod.name, index, lib.head)
+
+    out = ""
+    for filepath in filepaths:
+        with open(filepath, "r") as file:
+            lines = file.readlines()
+
+            lines = Unguard(lines)
+            lines = Uninclude(lines)
+
+            out += EMPTY.join(lines)
+    
+
+    with open(CONFIG_OUT_H, "w") as file:
+        file.write("#ifndef" + SPACE + CONFIG_GUARD + NEWLINE)
+        file.write("#define" + SPACE + CONFIG_GUARD + NEWLINE)
+
+        for libname in libnames:
+            file.write("#include" + SPACE + "<" + libname + ">" + NEWLINE)
+        
+        file.write(out + NEWLINE)
+        file.write("#endif" + SPACE  + "//" + SPACE + CONFIG_GUARD + NEWLINE)
+    print(out)
+
+def BuildSrc(mods: list[Mod]):
+    filepaths   = []
+    libnames    = []
+
+    LogTable()
+    for mod in mods:
+        for index, stage in enumerate(mod.stages):
+            for src in stage.files:
+                filepaths.append(src.file)
+                LogFile(mod.name, index, src.file)
+
+            for lib in stage.libs:
+                libnames.append(lib.head)
+                LogLib(mod.name, index, lib.head)
+
+    out = ""
+    for filepath in filepaths:
+        with open(filepath, "r") as file:
+            lines = file.readlines()
+
+            lines = Uninclude(lines)
+
+            out += EMPTY.join(lines)
+    
+
+    with open(CONFIG_OUT_H, "w") as file:
+        file.write("#include" + SPACE + QUOTE + CONFIG_OUT_H + QUOTE + NEWLINE)
+        for libname in libnames:
+            file.write("#include" + SPACE + "<" + libname + ">" + NEWLINE)
+        file.write(out + NEWLINE)
+    print(out)
+
+# ==== heads
+mod0 = Mod([
+    Stage([
+        Head("include/arg/cond/flag.h"),
+        Head("include/arg/cond/val.h")
+    ]),
+    Stage([
+        Head("include/arg/cond/condset.h")
+    ])
+], "arg/cond")
+
+mod1 = Mod([
+    Stage([
+        Head("include/arg/storage/flag.h"),
+        Head("include/arg/storage/datatype.h")
+    ])
+], "arg/storage")
+
+mod2 = Mod([
+    Stage([
+        Head("include/arg/init.h")
+    ])
+], "arg/init")
+
+mod3 = Mod([
+    Stage([
+        Head("include/arg/cond.h")
+    ])
+], "arg/cond")
+
+mod4 = Mod([
+    Stage([
+        Head("include/arg/find.h")
+    ])
+], "arg/find")
+
+mod5 = Mod([
+    Stage([
+        Head("include/input.h")
+    ]).dep("stdbool.h")
+], "input")
+
+mod6 = Mod([
+    Stage([
+        Head("include/password.h")
+    ])
+], "password")
+
+mod7 = Mod([
+    Stage([
+        Head("include/select.h")
+    ])
+], "select")
+
+# clear CLI
+os.system('cls' if os.name == 'nt' else 'clear')
+
+BuildHead([
+    mod0,
+    mod1,
+    mod2,
+    mod3,
+    mod4,
+    mod5,
+    mod6,
+    mod7
+])
