11fd36c65fa13069c69c89b79434a682d08b70f9
kod1ngclub <wiiwefwefbi@gmail.com>
2025-02-20 12:56:39 +0900

    FEAT: Implement utils or 'arg' modules with eq.h and has.h

--- src/arg/util.c
+++ src/arg/util.c
@@ -1,11 +1,20 @@
 #include "../../include/arg/util.h"
 
+#include "../../include/arg/model/expect.h"
+#include "../../include/arg/model/flag.h"
+#include "../../include/arg/model/val.h"
+#include "../../include/arg/model/stdarg.h"
+
+#include "../../include/arg/eq.h"
+#include "../../include/arg/has.h"
+
 int ccli_expect_eq1(
     const struct ccli_stdarg arg,
 
     const struct ccli_expectset s1
 ) {
-    return 0;
+    const struct ccli_expectset sets[1] = { s1 };
+    return ccli_expect_eq(arg, sets, 1);
 }
 
 int ccli_expect_eq2(
@@ -14,7 +23,8 @@ int ccli_expect_eq2(
     const struct ccli_expectset s1,
     const struct ccli_expectset s2
 ) {
-    return 0;
+    const struct ccli_expectset sets[2] = { s1, s2 };
+    return ccli_expect_eq(arg, sets, 2);
 }
 
 int ccli_expect_eq3(
@@ -24,7 +34,8 @@ int ccli_expect_eq3(
     const struct ccli_expectset s2,
     const struct ccli_expectset s3
 ) {
-    return 0;
+    const struct ccli_expectset sets[3] = { s1, s2, s3 };
+    return ccli_expect_eq(arg, sets, 3);
 }
 
 int ccli_expect_eq4(
@@ -35,7 +46,8 @@ int ccli_expect_eq4(
     const struct ccli_expectset s3,
     const struct ccli_expectset s4
 ) {
-    return 0;
+    const struct ccli_expectset sets[4] = { s1, s2, s3, s4 };
+    return ccli_expect_eq(arg, sets, 4);
 }
 
 int ccli_expect_eq5(
@@ -47,7 +59,8 @@ int ccli_expect_eq5(
     const struct ccli_expectset s4,
     const struct ccli_expectset s5
 ) {
-    return 0;
+    const struct ccli_expectset sets[5] = { s1, s2, s3, s4, s5 };
+    return ccli_expect_eq(arg, sets, 5);
 }
 
 int ccli_expect_eq6(
@@ -60,7 +73,8 @@ int ccli_expect_eq6(
     const struct ccli_expectset s5,
     const struct ccli_expectset s6
 ) {
-    return 0;
+    const struct ccli_expectset sets[6] = { s1, s2, s3, s4, s5, s6 };
+    return ccli_expect_eq(arg, sets, 6);
 }
 
 int ccli_expect_eq7(
@@ -74,7 +88,8 @@ int ccli_expect_eq7(
     const struct ccli_expectset s6,
     const struct ccli_expectset s7
 ) {
-    return 0;
+    const struct ccli_expectset sets[7] = { s1, s2, s3, s4, s5, s6, s7 };
+    return ccli_expect_eq(arg, sets, 7);
 }
 
 int ccli_expect_eq8(
@@ -89,7 +104,8 @@ int ccli_expect_eq8(
     const struct ccli_expectset s7,
     const struct ccli_expectset s8
 ) {
-    return 0;
+    const struct ccli_expectset sets[8] = { s1, s2, s3, s4, s5, s6, s7, s8 };
+    return ccli_expect_eq(arg, sets, 8);
 }
 
 // ==== utils of func expect_has
@@ -98,7 +114,8 @@ int ccli_expect_has1(
 
     const struct ccli_expectset s1
 ) {
-    return 0;
+    const struct ccli_expectset sets[1] = { s1 };
+    return ccli_expect_has(arg, sets, 1);
 }
 
 int ccli_expect_has2(
@@ -107,7 +124,8 @@ int ccli_expect_has2(
     const struct ccli_expectset s1,
     const struct ccli_expectset s2
 ) {
-    return 0;
+    const struct ccli_expectset sets[2] = { s1, s2 };
+    return ccli_expect_has(arg, sets, 2);
 }
 
 int ccli_expect_has3(
@@ -117,7 +135,8 @@ int ccli_expect_has3(
     const struct ccli_expectset s2,
     const struct ccli_expectset s3
 ) {
-    return 0;
+    const struct ccli_expectset sets[3] = { s1, s2, s3 };
+    return ccli_expect_has(arg, sets, 3);
 }
 
 int ccli_expect_has4(
@@ -128,28 +147,30 @@ int ccli_expect_has4(
     const struct ccli_expectset s3,
     const struct ccli_expectset s4
 ) {
-    return 0;
+    const struct ccli_expectset sets[4] = { s1, s2, s3, s4 };
+    return ccli_expect_has(arg, sets, 4);
 }
 
 // ==== utils for set
-struct ccli_expectset cset(
-    const struct ccli_expect* const expects,
-    const int size
-) {
-    return (struct ccli_expectset){ };
-}
-
 struct ccli_expectset cset1(
     const struct ccli_expect e1
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1 },
+        .size = 1,
+        .seq = false
+    };
 }
 
 struct ccli_expectset cset2(
     const struct ccli_expect e1,
     const struct ccli_expect e2
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1, e2 },
+        .size = 2,
+        .seq = false
+    };
 }
 
 struct ccli_expectset cset3(
@@ -157,7 +178,11 @@ struct ccli_expectset cset3(
     const struct ccli_expect e2,
     const struct ccli_expect e3
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1, e2, e3 },
+        .size = 3,
+        .seq = false
+    };
 }
 
 struct ccli_expectset cset4(
@@ -166,25 +191,33 @@ struct ccli_expectset cset4(
     const struct ccli_expect e3,
     const struct ccli_expect e4
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1, e2, e3, e4 },
+        .size = 4,
+        .seq = false
+    };
 }
 
 // ==== utils for seq
-struct ccli_expectset cseq(const struct ccli_expect* const expects, const int size) {
-    return (struct ccli_expectset){ };
-}
-
 struct ccli_expectset cseq1(
     const struct ccli_expect e1
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1 },
+        .size = 1,
+        .seq = true
+    };
 }
 
 struct ccli_expectset cseq2(
     const struct ccli_expect e1,
     const struct ccli_expect e2
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1, e2 },
+        .size = 2,
+        .seq = true
+    };
 }
 
 struct ccli_expectset cseq3(
@@ -192,7 +225,11 @@ struct ccli_expectset cseq3(
     const struct ccli_expect e2,
     const struct ccli_expect e3
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1, e2, e3 },
+        .size = 3,
+        .seq = true
+    };
 }
 
 struct ccli_expectset cseq4(
@@ -201,7 +238,11 @@ struct ccli_expectset cseq4(
     const struct ccli_expect e3,
     const struct ccli_expect e4
 ) {
-    return (struct ccli_expectset){ };
+    return (struct ccli_expectset){
+        .expects = { e1, e2, e3, e4 },
+        .size = 4,
+        .seq = true
+    };
 }
 
 // ==== utils for flag
@@ -209,19 +250,41 @@ struct ccli_expect flag(
     const char* const l,
     const char s
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .flag = (struct ccli_expect_flag){
+                .lflag = l,
+                .sflag = s
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_FLAG
+    };
 }
 
 struct ccli_expect lflag(
     const char* const l
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .lflag = (struct ccli_expect_long_flag){
+                .lflag = l
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_LFLAG
+    };
 }
 
 struct ccli_expect sflag(
     const char s
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .sflag = (struct ccli_expect_short_flag){
+                .sflag = s
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_SFLAG
+    };
 }
 
 
@@ -232,7 +295,16 @@ struct ccli_expect vstr(
     char* const out,
     const size_t size
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vstr = (struct ccli_expect_val_str){
+                .name = name,
+                .out = out,
+                .size = size
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_STR
+    };
 }
 
 struct ccli_expect vstr16(
@@ -240,7 +312,16 @@ struct ccli_expect vstr16(
 
     char* const out
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vstr = (struct ccli_expect_val_str){
+                .name = name,
+                .out = out,
+                .size = 16
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_STR
+    };
 }
 
 struct ccli_expect vstr32(
@@ -248,7 +329,16 @@ struct ccli_expect vstr32(
 
     char* const out
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vstr = (struct ccli_expect_val_str){
+                .name = name,
+                .out = out,
+                .size = 32
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_STR
+    };
 }
 
 struct ccli_expect vstr64(
@@ -256,7 +346,16 @@ struct ccli_expect vstr64(
 
     char* const out
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vstr = (struct ccli_expect_val_str){
+                .name = name,
+                .out = out,
+                .size = 64
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_STR
+    };
 }
 
 struct ccli_expect vstr128(
@@ -264,7 +363,16 @@ struct ccli_expect vstr128(
 
     char* const out
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vstr = (struct ccli_expect_val_str){
+                .name = name,
+                .out = out,
+                .size = 128
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_STR
+    };
 }
 
 struct ccli_expect vstr70(
@@ -272,7 +380,16 @@ struct ccli_expect vstr70(
 
     char* const out
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vstr = (struct ccli_expect_val_str){
+                .name = name,
+                .out = out,
+                .size = 70
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_STR
+    };
 }
 
 struct ccli_expect vchar(
@@ -280,7 +397,15 @@ struct ccli_expect vchar(
 
     char* const out
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vchar = (struct ccli_expect_val_char){
+                .name = name,
+                .out = out
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_CHAR
+    };
 }
 
 struct ccli_expect vint(
@@ -288,5 +413,13 @@ struct ccli_expect vint(
 
     int* const out
 ) {
-    return (struct ccli_expect){ };
+    return (struct ccli_expect){
+        .data = (union ccli_expect_data){
+            .vint = (struct ccli_expect_val_int){
+                .name = name,
+                .out = out
+            }
+        },
+        .type = CCLI_EXPECT_TYPE_VAL_INT
+    };
 }
